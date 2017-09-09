67c8a803f176ff6b62df9fe58251122a27bfa623
WincerChan <WincerChan@jxnu.edu.cn>
2017-09-09 20:23:07 +0800

    Add Flash Message

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, redirect, render_template, session, url_for
+from flask import Flask, flash, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_script import Manager
@@ -23,6 +23,9 @@ class NameForm(FlaskForm):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
