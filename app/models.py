b7fc506db4c42a25d31b627d03628bf4e9a5de91
WincerChan <WincerChan@jxnu.edu.cn>
2017-09-11 09:20:01 +0800

    Add Password hashing

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from . import db
 
 
@@ -19,3 +21,16 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {!r}>'.format(self.username)
+
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
