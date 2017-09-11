bb81a30d1729ce7939b81bb5061099e75c5dd074
WincerChan <WincerChan@jxnu.edu.cn>
2017-09-11 15:04:50 +0800

    Add edit-profile page

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
-from flask import current_app, redirect, render_template, session, url_for
+from flask import (current_app, flash, redirect, render_template, session,
+                   url_for)
+from flask_login import current_user, login_required
 
 from . import main
 from .. import db
 from ..email import send_email
 from ..models import User
-from .forms import NameForm
+from .forms import EditProfileForm, NameForm
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -41,3 +43,20 @@ def index():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
