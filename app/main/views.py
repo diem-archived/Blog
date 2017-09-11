1a4557a50668e5f1757935d86105c359da8e40d1
WincerChan <WincerChan@jxnu.edu.cn>
2017-09-11 22:45:22 +0800

    Add 8f, 8g, 8h

--- app/main/views.py
+++ app/main/views.py
@@ -1,43 +1,24 @@
-from datetime import datetime
-
-from flask import (current_app, flash, redirect, render_template, session,
-                   url_for)
+from flask import flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
 from . import main
 from .. import db
 from ..decorators import admin_required
-from ..email import send_email
-from ..models import Role, User
-from .forms import EditProfileAdminForm, EditProfileForm, NameForm
+from ..models import Permission, Post, Role, User
+from .forms import EditProfileAdminForm, EditProfileForm, PostForm
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-            if current_app.config['Blog_ADMIN']:
-                send_email(
-                    current_app.config['BLOG_ADMIN'],
-                    'New User',
-                    'mail/new_user',
-                    user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
+    form = PostForm()
+    if current_user.can(
+            Permission.WRITE_ARTICLES) and form.validate_on_submit():
+        post = Post(
+            body=form.body.data, author=current_user._get_current_object())
+        db.session.add(post)
         return redirect(url_for('.index'))
-    return render_template(
-        'index.html',
-        form=form,
-        name=session.get('name'),
-        known=session.get('known', False),
-        current_time=datetime.utcnow())
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
 
 
 @main.route('/user/<username>')
--- app/models.py
+++ app/models.py
@@ -40,6 +40,14 @@ class Role(db.Model):
         return '<Role {!r}>'.format(self.name)
 
 
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -55,6 +63,7 @@ class User(UserMixin, db.Model):
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     avatar_hash = db.Column(db.String(32))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -96,6 +105,44 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    def generate_reset_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'reset': self.id})
+
+    def reset_password(self, token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('reset') != self.id:
+            return False
+        self.password = new_password
+        db.session.add(self)
+        return True
+
+    def generate_email_change_token(self, new_email, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'change_email': self.id, 'new_email': new_email})
+
+    def change_email(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('change_email') != self.id:
+            return False
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        self.email = new_email
+        self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        db.session.add(self)
+        return True
+
     def can(self, permissions):
         return self.role is not None and (
             self.role.permissions & permissions) == permissions
@@ -111,7 +158,7 @@ class User(UserMixin, db.Model):
         if request.is_secure:
             url = 'https://secure.gravatar.com/avatar'
         else:
-            url = 'http://www.gravatar.com/avatar'
+            url = 'https://secure.gravatar.com/avatar'
         hash = self.avatar_hash or hashlib.md5(
             self.email.encode('utf-8')).hexdigest()
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
